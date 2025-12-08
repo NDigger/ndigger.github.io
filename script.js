c1ff27ecaefc47afa0a5f0c454ea670b6a862668
NDagger <yartsiz@gmail.com>
2025-12-08 20:01:40 +0100

    ratings coloring code removed

--- script.js
+++ script.js
@@ -15,30 +15,3 @@ toolSpans.forEach(span => {
         audioManager.resetPlayHover(audioManager.sounds.hover);
     });
 })
-
-// Ratings coloring
-const ratingSpans = document.querySelectorAll(".rating");
-
-const changeSpanStyleByRating = (span) => {
-    const rating = span.textContent;
-    const parent = span.parentElement;
-    const convertedRating = (Number(rating) - 1) * 30;
-    if (rating <= 1) {
-        parent.style.color = `hsl(0, 50%, 50%)`;
-        span.style.color = `hsl(0, 50%, 50%)`;
-        parent.style.backgroundColor = `hsla(0, 50%, 50%, 0.1)`;
-        parent.style.border = `2px outset hsl(0, 50%, 50%)`;
-    } else if (rating <= 4.5) {
-        parent.style.color = `hsl(${convertedRating}, 50%, 40%)`;
-        span.style.color = `hsl(${convertedRating}, 50%, 40%)`;
-        parent.style.backgroundColor = `hsla(${convertedRating}, 50%, 50%, 0.1)`;
-        parent.style.border = `2px outset hsl(${convertedRating}, 50%, 50%)`;
-    } else {
-        parent.style.border = "2px outset gold";
-        parent.style.color = 'gold';
-        span.style.color = 'gold';
-    }
-    
-}
-
-ratingSpans.forEach(span => changeSpanStyleByRating(span))

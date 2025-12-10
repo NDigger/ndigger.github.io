f61feadf5845c649df995a6bfc0785c1a19225d2
NDagger <yartsiz@gmail.com>
2025-12-10 16:26:24 +0100

    script.js cleanup

--- script.js
+++ script.js
@@ -4,18 +4,7 @@ import './status.js';
 import './buttons.js';
 import './windows.js';
 
-// Tool spans hover SFX 
-const toolSpans = [
-  ...Array.from(document.getElementById('tools-container').children),
-  ...Array.from(document.getElementById('pl-container').children)
-];
-
 window.addEventListener('load', () => {
   Array.from(document.querySelectorAll('.hover-sound'))
-  .forEach(el => {
-    el.addEventListener('mouseover', () => {
-      console.log(1)
-      audioManager.resetPlayHover(audioManager.sounds.hover);
-    })
-  })
+  .forEach(el => el.addEventListener('mouseover', () => audioManager.resetPlayHover(audioManager.sounds.hover)))
 })
